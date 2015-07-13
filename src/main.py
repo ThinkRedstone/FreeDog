148d775119e0099b6d2c18376095940680be24c0
ThinkRedstone <ittai11@gmail.com>
2015-07-13 14:21:48 +0300

    Added main, file in charge of operating all the other files

---
+++ src/main.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python
+
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "thinkredstone"
+__date__ = "$Jul 13, 2015 2:03:09 PM$"
+from gps.gpsData import startGPS, getLongitude, getLatitude
+
+if __name__ == "__main__":
+    startGPS()
+    print getLatitude()
+    print getLatitude()
+    
