9b5d638b4c7017c6b6b2b8c663ba54f9148438f8
ThinkRedstone <ittai11@gmail.com>
2015-08-21 20:51:06 +0300

    Implemented engine control in main

--- src/main.py
+++ src/main.py
@@ -6,21 +6,30 @@
 
 __author__ = "thinkredstone"
 __date__ = "$Jul 13, 2015 2:03:09 PM$"
-from gpsProcessing.gpsData import startGPS, getLongitude, getLatitude, closeGPS
+from gpsProcessing.gpsData import *
 from placment.circle import updatePosition
+from server.connector import *
+from control.engines import *
 import os
 from time import sleep
 
 if __name__ == "__main__":
     startGPS()
+    startConnection()
     try:
         while True:
             os.system('clear')
             print 'Long: ', getLongitude()
             print 'Lat: ', getLatitude()
+            updateUser(getUserLongtitude(),getUserLatitude())
+            turnRadius = getDistance()
             direction = updatePosition(getLongitude(),getLatitude())
             print 'Turn: ', direction
-            sleep(1)
+            if(direction is 'left' || getCommand() is 'TURN_LEFT':
+                turnLeft()
+            if(direction is 'right' || getCommand is 'TURN_RIGHT':
+                turnRight()
     except(KeyboardInterrupt, SystemExit):
+        closeConnection()
         closeGPS()
         print 'Exiting...'
