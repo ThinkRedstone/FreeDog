0d26efb6c3313faba44ca2bcba74aafa82e51644
ThinkRedstone <ittai11@gmail.com>
2015-11-09 23:17:24 +0200

    Light control now deals with calls before thread finished

--- src/control/engines.py
+++ src/control/engines.py
@@ -20,23 +20,27 @@ class ConnectionLight(Thread):
         Thread.__init__(self)
         
     def run(self):
+        self.running = True
         global connectionLed
         val = True
         for x in range(0, 2):
             sleep(1)
             val = not val
             GPIO.output(connectionLed, val)
+        self.running = False
 
 class RunningLight(Thread):
     def __init__(self):
         Thread.__init__(self)
         
     def run(self):
+        self.running = True
         global runningLed
         GPIO.output(runningLed, True)
         sleep(1)
         GPIO.output(runningLed, False)
         sleep(5)
+        self.running = False
 
 def turnLeft():
     GPIO.output(19, True)
@@ -57,9 +61,21 @@ def turnRight():
     time.sleep(10)
 
 def runningLights():
-    lightControl = runningLights()
-    lightControl.start()
+    global runLightThread
+    try:
+        if not runLightThread.running:
+            runLightThread = RunningLight()
+            runLightThread.start()
+    finally:
+        runLightThread = RunningLight()
+        runLightThread.start()
 
 def socketConnectedLights():
-    lightControl = ConnectionLight()
-    lightControl.start()
+    global connectionLightThread
+    try:
+        if not connectionLightThread.running:
+            connectionLightThread = ConnectionLight()
+            connectionLightThread.start()
+    finally:
+        connectionLightThread = ConnectionLights()
+        connectionLightThread.start()
