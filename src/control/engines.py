155377981b0b6bf1fa9a4c6b8821b2e934de508c
ThinkRedstone <ittai11@gmail.com>
2015-11-09 23:02:01 +0200

    Implemented better light control

--- src/control/engines.py
+++ src/control/engines.py
@@ -1,39 +1,65 @@
 import RPi.GPIO as GPIO
+from threading import Thread
 import time
+
+
+connectionLed = 13
+runningLed = 14
+leftMotor = 19
+rightMotor = 16
 
 ##GPIO.cleanup()
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(13,GPIO.OUT)
-GPIO.setup(16,GPIO.OUT)
-GPIO.setup(19,GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
+GPIO.setup(19, GPIO.OUT)
+
+class ConnectionLight(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        
+    def run(self):
+        global connectionLed
+        val = True
+        for x in range(0, 2):
+            sleep(1)
+            val = not val
+            GPIO.output(connectionLed, val)
+
+class RunningLight(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        
+    def run(self):
+        global runningLed
+        GPIO.output(runningLed, True)
+        sleep(1)
+        GPIO.output(runningLed, False)
+        sleep(5)
 
 def turnLeft():
-	GPIO.output(19,True)
-	time.sleep(2)
-	GPIO.output(19,False)
-	GPIO.output(16,True)
-	time.sleep(2)
-	GPIO.output(16,False)
-	time.sleep(10)
+    GPIO.output(19, True)
+    time.sleep(2)
+    GPIO.output(19, False)
+    GPIO.output(16, True)
+    time.sleep(2)
+    GPIO.output(16, False)
+    time.sleep(10)
 
 def turnRight():
-	GPIO.output(19,True)
-	time.sleep(2)
-	GPIO.output(19,False)
-	GPIO.output(16,True)
-	time.sleep(2)
-	GPIO.output(16,False)
-	time.sleep(10)
+    GPIO.output(19, True)
+    time.sleep(2)
+    GPIO.output(19, False)
+    GPIO.output(16, True)
+    time.sleep(2)
+    GPIO.output(16, False)
+    time.sleep(10)
 
-def notConnectedToServer():
-        for x in range(0, 10):
-                sleep(0.2)
-                val = x%2==0
-                GPIO.output(13,val)
+def runningLights():
+    lightControl = runningLights()
+    lightControl.start()
 
-def socketConnected():
-        for x in range(0, 2):
-                sleep(1)
-                val = x%2==0
-                GPIO.output(13,val)
+def socketConnectedLights():
+    lightControl = ConnectionLight()
+    lightControl.start()
